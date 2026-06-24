87d6b7f829eccd2fbca36eccbf8def884683ae37
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-06-24 22:28:57 +0530

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for idx, val in enumerate(nums):
+            fixed = nums[idx]
+            lookup_sum = -(fixed)
+            left = idx+1
+            right = len(nums) - 1
+            while left < right:
+                left_right_sum = nums[left] + nums[right]
+                if left_right_sum < lookup_sum:
+                    left += 1
+                elif left_right_sum > lookup_sum:
+                    right -= 1
+                elif left_right_sum == lookup_sum:
+                    res.add(tuple([nums[left], nums[right], fixed]))
+                    left += 1
+                    right -= 1
+
+        return [list(x) for x in res]
+                    
+
+
+        

3bea0300436075f8f286f1c2181b8f51932828d5
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-07-20 13:57:24 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-11

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right-left) // 2
+            if nums[mid] > nums[right]: # means unsorted array
+                left = mid + 1
+            elif nums[mid] <= nums[right]: # means sorted array
+                right = mid # search the left space - number is b/w left and the mid (including mid)
+        return nums[right] # we return nums[right] since now left and right will be same so can do either lol  
+            

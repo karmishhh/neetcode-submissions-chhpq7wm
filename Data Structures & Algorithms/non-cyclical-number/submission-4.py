a9ce3486687fbde9e7ddbaba9a3a010ddb87c0e9
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-07-24 18:36:43 +0530

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findsum(self,n):
+        summ = 0
+        while n>0:
+            d = n%10
+            n = n//10
+            summ += d*d
+        return summ
+
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = n
+        while fast != 1:
+            slow = self.findsum(slow)
+            fast = self.findsum(fast)
+            fast = self.findsum(fast)
+            if slow == fast and slow != 1: # we entered a cycle
+                return False
+        return True

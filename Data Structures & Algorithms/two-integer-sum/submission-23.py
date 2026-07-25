b02194ed6264eb9b207efc05ed779e223225173e
Kartik Mishra <55404534+karmishhh@users.noreply.github.com>
2026-07-25 13:20:01 +0530

    Add: two-integer-sum - submission-23

---
+++ Data Structures & Algorithms/two-integer-sum/submission-23.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for idx, val in enumerate(nums):
+            diff = target - val
+            if diff in hashmap:
+                return [hashmap[diff], idx]
+            else:
+                hashmap[val] = idx
+        
